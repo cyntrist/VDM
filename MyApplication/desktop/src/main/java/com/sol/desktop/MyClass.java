03befb26948a0071ce7796372bd16b97d6194c97
Cynthia Tristán <cyntrist@ucm.es>
2025-09-18 15:45:49 +0200

    Update MyClass.java

--- MyApplication/desktop/src/main/java/com/sol/desktop/MyClass.java
+++ MyApplication/desktop/src/main/java/com/sol/desktop/MyClass.java
@@ -17,17 +17,21 @@ import javax.swing.WindowConstants;
 
 public class MyClass {
     public static void main(String[] args) {
+
+        // vventana
         JFrame frame = new JFrame("Desktop");
         frame.setSize(500, 500);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         frame.setLayout(new FlowLayout());
 
+        // elementos
         TextField field = new TextField("Hello World!");
         Button button = new Button("Send");
         frame.add(field);
         frame.add(button);
 
+        // interaccion
         MouseListener ml = new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
@@ -56,9 +60,5 @@ public class MyClass {
             }
         };
         button.addMouseListener(ml);
-    }
-
-    public void buttonPressed(MouseEvent e) {
-
     }
 }
