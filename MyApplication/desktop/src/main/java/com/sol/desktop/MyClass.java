bf1805d012de98dc539c53a26b10b3d626591070
Cynthia Tristán <cyntrist@ucm.es>
2025-09-18 15:28:34 +0200

    Update MyClass.java

--- MyApplication/desktop/src/main/java/com/sol/desktop/MyClass.java
+++ MyApplication/desktop/src/main/java/com/sol/desktop/MyClass.java
@@ -1,4 +1,12 @@
 package com.sol.desktop;
+// java.swing.JFrame (Crea nuestra ventana)
+// java.awt.Button (Componente que representa un botón)
+// java.awt.TextField (Componente que representa texto)
+// java.awt.event.MouseListener (Nos permite escuchar los eventos de ratón
+
 
 public class MyClass {
+    public static void main(String[] args) {
+
+    }
 }
